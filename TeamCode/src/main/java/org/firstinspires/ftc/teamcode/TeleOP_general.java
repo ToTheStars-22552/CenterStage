fbf725dee2caed0a355a21162f4049f15c954390
ToTheStars-22552 <144327702+ToTheStars-22552@users.noreply.github.com>
2023-11-23 23:26:04 +0200

    Prima Varianta Center Stage

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP_general.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="General", group="Linear Opmode")
+@Disabled
+
+public class TeleOP_general
+
+{
+
+
+
+
+}
